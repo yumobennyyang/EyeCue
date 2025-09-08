06a83e7ca0c94ce8805669957fd8cdc9d1bc77a8
yumobennyyang <123268757+yumobennyyang@users.noreply.github.com>
2025-09-08 16:28:37 -0400

    Initial Commit

---
+++ EyeCueTests/EyeCueTests.swift
@@ -0,0 +1,17 @@
+//
+//  EyeCueTests.swift
+//  EyeCueTests
+//
+//  Created by Benny Yang on 9/8/25.
+//
+
+import Testing
+@testable import EyeCue
+
+struct EyeCueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
