0c7be96904e124c0124c054fbdf27a6a656c2f6a
yumobennyyang <123268757+yumobennyyang@users.noreply.github.com>
2025-09-10 02:17:32 -0400

    multicam

---
+++ AVMultiCamPiP/AppDelegate.swift
@@ -0,0 +1,13 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+Implements the application delegate.
+*/
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+}
